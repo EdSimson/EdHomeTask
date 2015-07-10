f0a2b897ef0ddc91bc6fcf13e65664edd536b14f
Dmitrij Lesechenko <titaplar@gmail.com>
2015-07-10 10:09:10 +0300

    Prog.kiev.ua group 6 home task 1

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.hometask.prog.kiev.ua;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CustomerRuner customerRuner = new CustomerRuner();
+        customerRuner.customAction();
+
+        }
+}
